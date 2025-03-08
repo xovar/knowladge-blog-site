e4436c69d76f9623e746833adc7a6e2f3d8adb1c
FARHAN X0VAR <farhanx0var@FARHANs-MacBook-Air.local>
2025-03-08 15:41:56 +0600

    header section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <div className='max-w-[1280px] mx-auto mt-5 pb-5 border-b-1 border-b-[rgba(17, 17, 17, 0.15)]'>
+      <div className='max-w-[1280px] mx-auto mt-5 pb-5 border-b-1' style={{borderBottom: '2px solid rgba(17, 17, 17, 0.15)'}}>
        <Header/>
       </div>
-      <Blogs/>
+      {/* Blog Section Added */}
+      <div className='mt-[32px] flex justify-center'>
+        <div className='w-[1280px] flex'>
+          <div className='w-[845px'>
+            <Blogs/>
+          </div>
+          <div>
+            <Bookmarks />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
